4b2e993a9c22f4e01fdc2b609b9d9681c7b6c4f0
double0can <46379734+double0can@users.noreply.github.com>
2026-03-12 02:24:14 +0000

    implemented economy system

--- GameManager.gd
+++ GameManager.gd
@@ -3,14 +3,17 @@ extends Node
 signal livesChanged(currentLives)
 signal gameOver
 signal waveChanged(currentWave)
+signal moneyChanged(currentMoney)
 
 var lives = 20
 var currentWave = 1
+var money = 100
 
 func _ready() -> void:
 	# Emit the initial value so UI can update
 	livesChanged.emit(lives)
 	waveChanged.emit(currentWave)
+	moneyChanged.emit(money)
 
 func livesLost(amount: int) -> void:
 	lives -= amount
@@ -20,3 +23,18 @@ func livesLost(amount: int) -> void:
 	if lives <= 0:
 		gameOver.emit()
 		print("Game over!")
+
+func addMoney(amount: int) -> void:
+	money += amount
+	moneyChanged.emit(money)
+	print("money added, Total: $", money)
+
+func spendMoney(amount: int) -> bool:
+	if money >= amount:
+		money -= amount
+		moneyChanged.emit(money)
+		return true
+		
+	else:
+		print("Not enough monye")
+		return false
