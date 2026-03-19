27c9d0df8a6d207db5d43f2c35161511bc3bd199
double0can <46379734+double0can@users.noreply.github.com>
2026-03-19 02:08:27 +0000

    Map 2 made (Mud run)

--- Scenes/HUD/towerShopButton.gd
+++ Scenes/HUD/towerShopButton.gd
@@ -10,39 +10,39 @@ var ghost_tower: Node2D = null
 
 
 func _gui_input(event: InputEvent) -> void:
-	# Check if we clicked the Left Mouse Button
+	#Checks if left mouse pressed
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			# If we clicked DOWN, try to pick up a tower
+			#try pick up a tower
 			if GameManager.money >= towerPrice:
 				start_drag()
 			else:
 				print("Not enough money!")
 		else:
-			# If we let GO of the mouse, try to place it
+			# try to place tower
 			if is_dragging:
 				drop_tower()
 
 func start_drag() -> void:
 	is_dragging = true
 	
-	# Spawn a "Ghost" tower
+	#Spawns "Ghost" tower
 	ghost_tower = towerScene.instantiate()
 	get_tree().current_scene.add_child(ghost_tower)
 	
-	# Disable its shooting and timer so it doesn't fire while we hold it
+	#Disable shooting & timer so it doesnt shoot while holding it
 	ghost_tower.set_process(false)
 	ghost_tower.get_node("FireRate").stop()
 	
-	# Make it transparent
+	#makes transparent
 	ghost_tower.modulate = Color(1, 1, 1, 0.5)
 
 func _process(delta: float) -> void:
 	if is_dragging and ghost_tower != null:
-		# Make the ghost tower follow the mouse
+		#ghost tower followw mouse
 		ghost_tower.global_position = ghost_tower.get_global_mouse_position()
 		
-		# Check if it is on the path
+		#check if on the path
 		if is_invalid_location():
 			invalidTint.visible = true
 			ghost_tower.modulate = Color(1, 0, 0, 0.5) # Turn ghost red too
@@ -51,33 +51,33 @@ func _process(delta: float) -> void:
 			ghost_tower.modulate = Color(1, 1, 1, 0.5)
 
 func is_invalid_location() -> bool:
-	# Look at the hitbox we created earlier
+	#Look for hitbox
 	var hitbox = ghost_tower.get_node("placementHitbox")
 	
-	# Check everything the hitbox is currently touching
+	#check what hitbox is touching
 	for area in hitbox.get_overlapping_areas():
 		if area.is_in_group("path"):
-			return true # It IS touching the path
+			return true # if touching path
 			
-	return false # It is safe to build
+	return false #if safe to place
 
 func drop_tower() -> void:
 	is_dragging = false
 	invalidTint.visible = false
 	
 	if is_invalid_location():
-		# Invalid! Destroy the ghost. (Money isn't spent yet)
+		#Destroy the ghost Money isn't spent
 		ghost_tower.queue_free()
 		print("Cannot build on the path!")
 	else:
-		# Valid! Spend the money
+		#Spend the money
 		if GameManager.spendMoney(towerPrice):
-			# Turn it fully opaque and turn its script/timer back on!
+			# urn fully transparaent and turn script & timer back on
 			ghost_tower.modulate = Color(1, 1, 1, 1)
 			ghost_tower.set_process(true)
 			ghost_tower.get_node("FireRate").start()
 			
-			# Let go of the reference so it stays on the map permanently
+			#let go of the reference so it stays on the map permanently
 			ghost_tower = null 
 		else:
 			ghost_tower.queue_free()
--- Scenes/Main Menu/mainMenu.gd
+++ Scenes/Main Menu/mainMenu.gd
@@ -16,9 +16,22 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_map_1_button_pressed() -> void:
+	GameManager.lives = 20
+	GameManager.money = 100
+	GameManager.currentWave = 1
+	
 	#Loads map 1 (Back Roads)
 	get_tree().change_scene_to_file("res://Scenes/Map/map_1.tscn")
 
 func _on_back_button_pressed() -> void:
 	levelContainer.visible = false
 	mainContainer.visible = true
+
+
+func _on_map_2_button_pressed() -> void:
+	#incase they played on a different map before this one
+	GameManager.lives = 20
+	GameManager.money = 100
+	GameManager.currentWave = 1
+	
+	get_tree().change_scene_to_file("res://Scenes/Map2/Map2.tscn")
