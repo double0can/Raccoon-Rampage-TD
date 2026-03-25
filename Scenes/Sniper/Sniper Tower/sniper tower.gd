52af68b3adea00696f0dab6cf0f455884178967a
double0can <46379734+double0can@users.noreply.github.com>
2026-03-25 03:10:04 +0000

    Added fully functional sniper tower
    added a sniper tower that has more range and does more damage but fires slower

---
+++ Scenes/Sniper/Sniper Tower/sniper tower.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+@export var bulletScene: PackedScene
+
+var inRange: Array = []
+var currentTarget: Node2D = null
+
+@onready var weaponSprite: Sprite2D = $weaponSprite
+@onready var firePoint: Marker2D = $weaponSprite/firePoint
+@onready var sniper: Sprite2D = $SniperSprite
+
+
+
+
+func _process(delta: float) -> void:
+	update_target()
+	
+	if currentTarget:
+		weaponSprite.look_at(currentTarget.global_position)
+		
+		if currentTarget.global_position.x < global_position.x:
+			sniper.flip_h = true #flips raccoon to look left
+			weaponSprite.flip_v = true #flips gun vertically so it isnt upside down
+			
+		else:
+			#keeps sprites in deafualt facing position
+			sniper.flip_h = false
+			weaponSprite.flip_v = false
+
+func update_target():
+	#check if it target is still valid (not dead/despawned)
+	if currentTarget != null:
+		if not is_instance_valid(currentTarget):
+			currentTarget = null
+			#clean array if the enemy died while in range
+			inRange = inRange.filter(func(e): return is_instance_valid(e))
+	
+	#pick the first enemy in range if none is already targeted
+	if currentTarget == null and inRange.size() > 0:
+		currentTarget = inRange[0]
+
+#When something enters the range circle
+func _on_range_area_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		inRange.append(body)
+
+#When something exits the range circle
+func _on_range_area_exited(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		inRange.erase(body)
+		
+		#if enemy exiting was  target clear target
+		if body == currentTarget:
+			currentTarget = null
+
+
+func _on_fire_rate_timeout() -> void:
+	if currentTarget != null and is_instance_valid(currentTarget):
+		shoot()
+
+func shoot() -> void:
+	if bulletScene:
+		var newBullet = bulletScene.instantiate()
+		
+		get_tree().current_scene.add_child(newBullet)
+		newBullet.global_position = firePoint.global_position
+		newBullet.global_rotation = weaponSprite.global_rotation
---
+++ Scenes/Sniper/sniper bullet/sniper bullet.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var speed: float = 1000.0
+@export var damage: int = 3
+
+func _physics_process(delta: float) -> void:
+	# transform.x points where bullet is facing making it fly straight
+	global_position += transform.x * speed * delta
+
+func _on_area_entered(area: Node2D) -> void:
+	dealDamage(area)
+
+func dealDamage(target: Node2D) -> void:
+	if target.is_in_group("enemy"):
+		print("bullet hit")
+		
+		var enemyRoot = target.get_parent()
+		if enemyRoot.has_method("takeDamage"):
+			print("health script attached on parent")
+			enemyRoot.takeDamage(damage)
+			
+		else:
+			print("bullet hit but couldnt find takeDamage")
+			
+		queue_free()
